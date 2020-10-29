dba8d61e14e5e979f2490c9f7c8883870668dc85
Tanner Boyer <tannerjboyer@gmail.com>
2020-10-28 17:04:36 -0700

    deploying

---
+++ client/build/precache-manifest.0093f59fef662e40ec869acab6b132da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2f4aa7925238eb0e115",
+    "url": "/static/css/main.328016bb.chunk.css"
+  },
+  {
+    "revision": "a2f4aa7925238eb0e115",
+    "url": "/static/js/main.104612dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bf2f30d9956321cad200",
+    "url": "/static/css/2.8ce0415d.chunk.css"
+  },
+  {
+    "revision": "bf2f30d9956321cad200",
+    "url": "/static/js/2.784e6b20.chunk.js"
+  },
+  {
+    "revision": "e953bf5e0b375e6e6fbad27a9a820261",
+    "url": "/static/media/space.e953bf5e.jpg"
+  },
+  {
+    "revision": "5a996d195eb0fe7e1db8b5bbee04b471",
+    "url": "/index.html"
+  }
+];
